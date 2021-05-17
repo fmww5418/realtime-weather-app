aa20e92128dc348a6d053db163e3e7ed4ba045a9
fmww5418 <fmww5418@gmail.com>
2021-05-17 23:07:02 +0800

    First Version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import WeatherApp from "./WeatherApp";
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// 這支 CSS 檔的樣式會作用到全域
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <WeatherApp />
+  </StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/useWeatherApi.jsx
@@ -0,0 +1,106 @@
+import { useState, useEffect, useCallback } from "react";
+
+const fetchWeatherForecast = (cityName) => {
+  return fetch(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-6436EE62-DAE9-4ED0-870A-989718AE0D86&locationName=${cityName}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const locationData = data.records.location[0];
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (["Wx", "PoP", "CI"].includes(item.elementName)) {
+            neededElements[item.elementName] = item.time[0].parameter;
+          }
+          return neededElements;
+        },
+        {}
+      );
+
+      return {
+        description: weatherElements.Wx.parameterName,
+        weatherCode: weatherElements.Wx.parameterValue,
+        rainPossibility: weatherElements.PoP.parameterName,
+        comfortability: weatherElements.CI.parameterName
+      };
+    });
+};
+
+const fetchCurrentWeather = (locationName) => {
+  return fetch(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-6436EE62-DAE9-4ED0-870A-989718AE0D86&locationName=${locationName}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const locationData = data.records.location[0];
+
+      // STEP 2：將風速（WDSD）、氣溫（TEMP）和濕度（HUMD）的資料取出
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (["WDSD", "TEMP", "HUMD"].includes(item.elementName)) {
+            neededElements[item.elementName] = item.elementValue;
+          }
+          return neededElements;
+        },
+        {}
+      );
+
+      return {
+        observationTime: locationData.time.obsTime,
+        locationName: locationData.locationName,
+        description: "多雲時晴",
+        temperature: weatherElements.TEMP,
+        windSpeed: weatherElements.WDSD,
+        humid: weatherElements.HUMD
+      };
+    });
+};
+
+const useWeatherApi = (currentLocation) => {
+  const { locationName, cityName } = currentLocation;
+
+  const [weatherElement, setWeatherElement] = useState({
+    observationTime: new Date(),
+    locationName: "",
+    humid: 0,
+    temperature: 0,
+    windSpeed: 0,
+    description: "",
+    weatherCode: 0,
+    rainPossibility: 0,
+    comfortability: "",
+    isLoading: true
+  });
+
+  const fetchData = useCallback(() => {
+    // STEP 3：把原本的 fetchData 改名為 fetchingData 放到 useCallback 的函式內
+    const fetchingData = async () => {
+      const [currentWeather, weatherForecast] = await Promise.all([
+        fetchCurrentWeather(locationName),
+        fetchWeatherForecast(cityName)
+      ]);
+
+      setWeatherElement({
+        ...currentWeather,
+        ...weatherForecast,
+        isLoading: false
+      });
+    };
+
+    setWeatherElement((prevState) => ({
+      ...prevState,
+      isLoading: true
+    }));
+    // STEP 4：記得要呼叫 fetchingData 這個方法
+    fetchingData();
+    // STEP 5：因為 fetchingData 沒有相依到 React 組件中的資料狀態，所以 dependencies 陣列中不帶入元素
+  }, [locationName, cityName]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return [weatherElement, fetchData];
+};
+
+export default useWeatherApi;
